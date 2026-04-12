2a1f6571d6cc80f864ac757928841cb7edde4740
YJPatel1309 <yugpatel091310@gmail.com>
2026-04-12 20:51:52 +0530

    Update script.cs

--- script.cs
+++ script.cs
@@ -1,1 +1,8 @@
+const firebaseConfig = { 
+    apiKey: "AIzaSyCDwmQT8q_gL8TxFW8Atdl9JtRo3ywYj98", 
+    databaseURL: "https://chat-go12-default-rtdb.firebaseio.com/", 
+    storageBucket: "chat-go12.firebasestorage.app", 
+    projectId: "chat-go12" 
+};
+const nodeName = "NODE_03";
 
